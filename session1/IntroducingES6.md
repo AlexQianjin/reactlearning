ab5097f88a0d05ddf69d6f9a69dfa16cab5cb095
Alex Qin <qianjin.qin@qq.com>
2018-03-02 19:25:55 +0800

    add Introducing ES6

---
+++ session1/IntroducingES6.md
@@ -0,0 +1,12 @@
+# ES6
+## 1. Declaring Variables in ES6
+- const
+- let
+- Template Strings
+- Default Parameters
+## 2. Arrow Functions
+## 3. Transpiling ES6
+- Babel
+## ES6 Objects and Arrays
+- Destructuring Assignment
+- Object Literal Enhancement
---
+++ session1/example.js
@@ -0,0 +1,163 @@
+// ====================================================================
+// Declaring Variables in ES6
+// ====================================================================
+
+// const
+var pizza = true;
+pizza = false;
+console.log(pizza); //false
+
+const pizza = true;
+pizza = false;
+
+// let
+var topic = 'Javascript';
+
+if (topic) {
+	var topic = 'React';
+	console.log('block', topic); // block React
+}
+
+console.log('global', topic); // global React
+
+// -------
+var topic = 'JavaScript';
+if (topic) {
+	let topic = 'React'
+	console.log('block', topic) // React
+}
+console.log('global', topic) // JavaScript
+
+// Template Strings
+let lastName = 'Qin';
+let firstName = 'Alex';
+let middleName = 'Super';
+
+console.log(lastName + ", " + firstName + " " + middleName);
+console.log(`${lastName}, ${firstName} ${middleName}`);
+
+// Default Parameters
+function logActivity(name="Shane McConkey", activity="skiing") {
+	console.log( `${name} loves ${activity}` );
+}
+
+var defaultPerson = {
+	name: {
+		first: "Shane",
+		last: "McConkey"
+	},
+	favActivity: "skiing"
+};
+
+function logActivity(p=defaultPerson) {
+	console.log(`${p.name.first} loves ${p.favActivity}`)
+}
+
+// ====================================================================
+// Arrow Functions
+// ====================================================================
+var lordify = function(firstname) {
+	return `${firstname} of Canterbury`;
+};
+console.log( lordify("Dale") ); // Dale of Canterbury
+console.log( lordify("Daryle") ); // Daryle of Canterbury
+
+var lordify = firstname => `${firstname} of Canterbury`;
+
+// Old
+var lordify = function(firstName, land) {
+	return `${firstName} of ${land}`;
+}
+// New
+var lordify = (firstName, land) => `${firstName} of ${land}`;
+console.log( lordify("Dale", "Maryland") ); // Dale of Maryland
+console.log( lordify("Daryle", "Culpeper") ); // Daryle of Culpeper
+
+// ====================================================================
+// ES6 Objects and Arrays
+// ====================================================================
+
+// Destructuring Assignment
+// ------ assignment ------
+var sandwich = {
+	bread: "dutch crunch",
+	meat: "tuna",
+	cheese: "swiss",
+	toppings: ["lettuce", "tomato", "mustard"]
+};
+var {bread, meat} = sandwich;
+console.log(bread, meat); // dutch crunch tuna
+
+var {bread, meat} = sandwich;
+bread = "garlic";
+meat = "turkey";
+console.log(bread); // garlic
+console.log(meat); // turkey
+console.log(sandwich.bread, sandwich.meat); // dutch crunch tuna
+
+// ------ arguments ------
+var lordify = regularPerson => {
+	console.log(`${regularPerson.firstname} of Canterbury`)
+};
+var regularPerson = {
+	firstname: "Bill",
+	lastname: "Wilson"
+};
+lordify(regularPerson); // Bill of Canterbury
+
+var lordify = ({firstname}) => {
+	console.log(`${firstname} of Canterbury`)
+};
+lordify(regularPerson); // Bill of Canterbury
+
+// ------ arrays ------
+var [firstResort] = ["Kirkwood", "Squaw", "Alpine"];
+console.log(firstResort); // Kirkwood
+
+var [,,thirdResort] = ["Kirkwood", "Squaw", "Alpine"]
+console.log(thirdResort) // Alpine
+
+// Object Literal Enhancement
+// ------ object ------ 
+var name = "Tallac";
+var elevation = 9738;
+var funHike = {name, elevation};
+console.log(funHike); // {name: "Tallac", elevation: 9738}
+
+// ------ function ------ 
+var name = "Tallac";
+var elevation = 9738;
+var print = function() {
+	console.log(`Mt. ${this.name} is ${this.elevation} feet tall`);
+}
+var funHike = {name, elevation, print};
+funHike.print(); // Mt. Tallac is 9738 feet tall
+
+// ------ Old versus new: Object syntax ------
+// OLD
+var skier = {
+	name: name,
+	sound: sound,
+	powderYell: function() {
+		var yell = this.sound.toUpperCase();
+		console.log(`${yell} ${yell} ${yell}!!!`);
+	},
+	speed: function(mph) {
+		this.speed = mph;
+		console.log('speed:', mph);
+	}
+};
+
+// NEW
+const skier = {
+	name,
+	sound,
+	powderYell() {
+		let yell = this.sound.toUpperCase();
+		console.log(`${yell} ${yell} ${yell}!!!`);
+	},
+	speed(mph) {
+		this.speed = mph;
+		console.log('speed:', mph);
+	}
+}
