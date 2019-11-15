827361307c3c814e8ca056de747a2bc9b4971072
Alex Qin <qianjin.qin@qq.com>
2019-11-15 16:31:09 +0800

    update installing docker script

--- linux/install/docker.sh
+++ linux/install/docker.sh
@@ -13,8 +13,9 @@
 sudo apt-get update
 sudo apt-get install apt-transport-https ca-certificates curl gnupg-agent software-properties-common
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+sudo apt-key fingerprint 0EBFCD88
 # sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
-sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu bionic stable"
+sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu Disco stable"
 sudo apt-get update
 # apt-cache policy docker-ce
 apt-cache madison docker-ce
