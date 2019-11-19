fe2f621987c2b7cd795359a75ef594876e85643b
AlexQin <qianjin.qin@qq.com>
2019-11-19 10:52:02 +0800

    update docker-compose / add kubectl and minikube

---
+++ linux/install/minikube.sh
@@ -0,0 +1,7 @@
+# curl -Lo minikube https://storage.googleapis.com/minikube/releases/latest/minikube-linux-amd64 \
+#  && chmod +x minikube
+# sudo mkdir -p /usr/local/bin/
+# sudo install minikube /usr/local/bin/
+
+ curl -LO https://storage.googleapis.com/minikube/releases/latest/minikube-linux-amd64 \
+   && sudo install minikube-linux-amd64 /usr/local/bin/minikube
